00ea70491ea1eb86425106c660c39a1fe45c4b56
FanOfStars <ramazanovapea@gmail.com>
2025-10-08 21:53:45 +0300

    Перенос основных виджетов

--- lib/CollumScreen.dart
+++ lib/CollumScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Elements.dart';
 
 class ColumnScreen extends StatefulWidget {
   const ColumnScreen({super.key});
@@ -120,108 +121,6 @@ class _ColumnScreenState extends State<ColumnScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class ElementIDEF extends StatelessWidget {
-  final String code;
-  final String content;
-  final bool selected;
-  final ValueChanged<bool?> onChanged;
-
-  const ElementIDEF({
-    super.key,
-    required this.code,
-    required this.content,
-    required this.selected,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      child: Row(
-        children: [
-          Checkbox(value: selected, onChanged: onChanged),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  code,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  content,
-                  style: const TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class LeftRoundedButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-
-  const LeftRoundedButton({
-    super.key,
-    required this.text,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.all(8),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(12),
-            bottomLeft: Radius.circular(12),
-          ),
-        ),
-      ),
-      onPressed: onPressed,
-      child: Text(text),
-    );
-  }
-}
-
-class RightRoundedButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-
-  const RightRoundedButton({
-    super.key,
-    required this.text,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.all(8),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(12),
-            bottomRight: Radius.circular(12),
-          ),
-        ),
-      ),
-      onPressed: onPressed,
-      child: Text(text),
     );
   }
 }
